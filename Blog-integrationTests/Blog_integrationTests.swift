e01d52d1992162cc26b853050a5050ddff1e5f4e
silver12666 <32123288+silver12666@users.noreply.github.com>
2018-01-08 14:10:51 +0200

    Lab 3, task 3. Test MappableTableVC behavior

--- Blog-integrationTests/Blog_integrationTests.swift
+++ Blog-integrationTests/Blog_integrationTests.swift
@@ -11,26 +11,37 @@ import XCTest
 
 class Blog_integrationTests: XCTestCase {
     
+    var blogVC: MyBlogVC?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        blogVC = MyBlogVC()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        blogVC = nil
+        
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMappableVC() {
+        
+        XCTAssert(blogVC!.mappables.isEmpty)
+        
+        blogVC!.mappables.append(PostModel())
+        
+        XCTAssert(blogVC!.mappables.count == 1)
+        
+        (0..<10).forEach({ _ in blogVC!.mappables.append(PostModel()) })
+        
+        XCTAssert(blogVC!.mappables.count == 11)
+        XCTAssertFalse(blogVC!.mappables.isEmpty)
+        
+        blogVC!.mappables.removeAll()
+        
+        XCTAssertTrue(blogVC!.mappables.isEmpty)
     }
     
 }
